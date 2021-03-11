276fcf1111a7aa47cbaa3ae9343bdaf12bc6346a
Vladyslav <vladlazaruk@gmail.com>
2021-03-11 11:20:58 +0100

    added try/catch

--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -59,11 +59,12 @@ export default class MiniSlider extends Slider {
     }
 
     init() {
+      try{
         this.container.style.cssText = `
-            display: flex;
-            flex-wrap: wrap;
-            overflow: hidden;
-            align-items: flex-start;
+        display: flex;
+        flex-wrap: wrap;
+        overflow: hidden;
+        align-items: flex-start;
         `;
 
         this.bindTriggers();
@@ -72,5 +73,6 @@ export default class MiniSlider extends Slider {
         if (this.autoplay) {
             setInterval(() => this.nextSlide(), 5000);
         }
+      }catch(e) {}
     }
 }
